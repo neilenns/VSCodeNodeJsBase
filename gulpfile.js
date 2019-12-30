81d44b6388c340d191a640331676b801d36356d8
Neil Enns <info@danecreekphotography.com>
2019-12-30 15:54:13 -0800

    Debugging works
    Adds in sourcemap generation and the launch and build tasks for VS Code.

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,14 @@ var typeScriptOptions = {
     target: "es5"
 };
 
+var sourcemapsOptions = {
+    sourceRoot: "../src/"
+};
+
 gulp.task('default', function () {
     return gulp.src("src/**/*.ts")
         .pipe(sourcemaps.init())
         .pipe(ts(typeScriptOptions))
-        .pipe(sourcemaps.write('.'))
+        .pipe(sourcemaps.write(sourcemapsOptions))
         .pipe(gulp.dest('dist'));
 });
