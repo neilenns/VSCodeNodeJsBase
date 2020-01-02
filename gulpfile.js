5dea81d149b40d07a5ec458131d2e63909bd2eaa
Neil Enns <info@danecreekphotography.com>
2020-01-01 18:54:18 -0800

    Add incremental build to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var sourcemaps = require("gulp-sourcemaps");
 
 var typeScriptOptions = {
-    noImplicitAny: true,
-    target: "es5"
+  noImplicitAny: true,
+  target: "es5"
 };
 
 var sourcemapsOptions = {
-    sourceRoot: "../src/"
+  sourceRoot: "../src/"
 };
 
-gulp.task('default', function () {
-    return gulp.src("src/**/*.ts")
-        .pipe(sourcemaps.init())
-        .pipe(ts(typeScriptOptions))
-        .pipe(sourcemaps.write(sourcemapsOptions))
-        .pipe(gulp.dest('dist'));
+gulp.task("default", function() {
+  return gulp
+    .src("src/**/*.ts", { since: gulp.lastRun("default") })
+    .pipe(sourcemaps.init())
+    .pipe(ts(typeScriptOptions))
+    .pipe(sourcemaps.write(sourcemapsOptions))
+    .pipe(gulp.dest("dist"));
 });
